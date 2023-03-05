3e6b7c729c167aaaf37189b635930c31e4ef6580
Dmitry Strizhakov <d.strizhakov@gmail.com>
2023-03-05 21:37:17 +0300

    04-dom-attributes-and-properties

---
+++ document/03-basic-dom-node-properties/04-task.js
@@ -0,0 +1,5 @@
+// Где в DOM-иерархии "document"?
+// важность: 4
+// Объектом какого класса является document? //? HTMLDocument
+// Какое место он занимает в DOM-иерархии? // ? HTMLDocument - Document - Node
+// Наследует ли он от Node или от Element, или может от HTMLElement? // ? наследует от Node -->
