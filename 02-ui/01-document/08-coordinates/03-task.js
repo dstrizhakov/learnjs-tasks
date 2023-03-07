c74bab7367e041178be9256e9eeb3c731aafa211
Dmitry Strizhakov <d.strizhakov@gmail.com>
2023-03-07 15:36:32 +0300

    01-introduction-browser-events

--- 02-ui/01-document/08-coordinates/03-task.js
+++ 02-ui/01-document/08-coordinates/03-task.js
@@ -1,1 +1,23 @@
+function positionAt(anchor, position, elem) {
+  let coords = anchor.getBoundingClientRect();
 
+  if (position == "top-out") {
+    elem.style.left = coords.left + "px";
+    elem.style.top = coords.top - elem.offsetHeight + "px";
+  } else if (position == "top-in") {
+    elem.style.left = coords.left + "px";
+    elem.style.top = coords.top + "px";
+  } else if (position == "right-out") {
+    elem.style.left = coords.right + "px";
+    elem.style.top = coords.top + "px";
+  } else if (position == "right-in") {
+    elem.style.left = coords.right - elem.offsetWidth + "px";
+    elem.style.top = coords.top + "px";
+  } else if (position == "bottom-out") {
+    elem.style.left = coords.left + "px";
+    elem.style.top = coords.bottom + "px";
+  } else if (position == "bottom-in") {
+    elem.style.left = coords.left + "px";
+    elem.style.top = coords.bottom - elem.offsetHeight + "px";
+  }
+}
