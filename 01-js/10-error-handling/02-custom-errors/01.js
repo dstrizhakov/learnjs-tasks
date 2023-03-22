74396c306303148b4d1c71ddb6528fb336c47144
Dmitry Strizhakov <d.strizhakov@gmail.com>
2023-03-22 21:32:05 +0300

    02-custom-errors

---
+++ 01-js/10-error-handling/02-custom-errors/01.js
@@ -0,0 +1,24 @@
+/*
+Наследование от SyntaxError
+важность: 5
+Создайте класс FormatError, который наследует от встроенного класса SyntaxError.
+Класс должен поддерживать свойства message, name и stack.
+Пример использования:
+*/
+
+class FormatError extends SyntaxError {
+	constructor(message){
+		super(message);
+		this.name = "FormatError"
+	}
+}
+
+
+let err = new FormatError("ошибка форматирования");
+
+console.log( err.message ); // ошибка форматирования
+console.log( err.name ); // FormatError
+console.log( err.stack ); // stack
+
+console.log( err instanceof FormatError ); // true
+console.log( err instanceof SyntaxError ); // true (потому что наследует от SyntaxError)
